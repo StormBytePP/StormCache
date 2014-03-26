c2f0a224dd937f650703f1107813805e9fd648bd
David Carlos Manuelda <stormbyte@gmail.com>
2014-03-26 06:57:45 +0100

    Added documentation in HTML for StormCache

---
+++ documentation/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Exception"],["c","PoolCantDeleteDefault"],["c","PoolItemNotFound"],["c","PoolNameConflict"],["c","PoolNotConnected"],["c","PoolNotFound"],["c","PoolServerConflict"],["c","StormCache"]];
